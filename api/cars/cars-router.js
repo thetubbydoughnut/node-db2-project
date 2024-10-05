9b9a81777addaccb06445dfd69d322a47e93f867
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-05 16:01:29 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,13 +1,24 @@
 // DO YOUR MAGIC
 const express = require('express')
 
+const Car = require('./cars-model')
+
+const {
+    checkCarId,
+} = require('./cars-middleware')
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
-router.get('/:id', async (req, res, next) => {
-    res.json(`getting car with id ${req.params.id}`)
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 router.post('/', async (req, res, next) => {
     res.json('posting new car')
