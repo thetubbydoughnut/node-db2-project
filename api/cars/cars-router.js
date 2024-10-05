836bea6de7c772fb17a6ed758d16798804821b11
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-05 16:05:50 -0500

    wiring looks good

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -5,6 +5,9 @@ const Car = require('./cars-model')
 
 const {
     checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
 } = require('./cars-middleware')
 
 const router = express.Router()
@@ -20,7 +23,11 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', checkCarId, async (req, res, next) => {
     res.json(req.car)
 })
-router.post('/', async (req, res, next) => {
+router.post('/',
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    async (req, res, next) => {
     res.json('posting new car')
 })
 
