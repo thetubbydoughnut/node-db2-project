6d4bbc3d39d3ffb7d5f440c4cd9e343c5d64187f
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-05 11:40:44 -0500

    i miss spelled a async now its fixed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -23,7 +23,7 @@ const cars = [
     },
 ]
 
-exports.seed = aync function (knex) {
+exports.seed = async function (knex) {
     await knex('cars').truncate()
     await knex('cars').insert(cars)
 } 
