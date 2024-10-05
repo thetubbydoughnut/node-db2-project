06da085ebb2302aeefcfdbefb0417e18f34d79e2
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-05 16:14:44 -0500

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -15,6 +15,22 @@ const checkCarId = async (req, res, next) => {
 }
 
 const checkCarPayload = (req, res, next) => {
+  if (!req.body.vin) return next({
+     status: 400,
+     message: 'vin is missing',
+  })
+  if (!req.body.make) return next({
+    status: 400,
+    message: 'make is missing',
+  })
+  if (!req.body.model) return next({
+    status: 400,
+    message: 'model is missing',
+  })
+  if (!req.body.mileage) return next({
+    status: 400,
+    message: 'mileage is missing',
+  })
   next()
 }
 
